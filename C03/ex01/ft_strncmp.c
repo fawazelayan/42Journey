9781dfab62ac3709f76f317abafad168d2e01d0b
Fawaz <fawaz.elayan6@gmail.com>
2024-07-29 21:39:15 +0300

    Solved 100%

---
+++ C03/ex01/ft_strncmp.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felayan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/28 22:54:15 by felayan           #+#    #+#             */
+/*   Updated: 2024/07/29 16:04:31 by felayan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+
+int	ft_strncmp(char *s1, char *s2, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
+	{
+		if (s1[i] > s2[i])
+			return (s1[i] - s2[i]);
+		else if (s1[i] < s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (0);
+}
+/*#include <stdio.h>
+int	main(void)
+{
+	int i = ft_strncmp("", "", 7);
+	printf("%i", i);
+}
+*/

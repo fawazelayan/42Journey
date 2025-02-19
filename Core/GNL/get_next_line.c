8286b330432eb380898671074a03a31f36a97f94
fawazelayan <fawaz.elayan6@gmail.com>
2025-02-19 03:07:02 +0300

    ADDED GNL

---
+++ Core/GNL/get_next_line.c
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felayan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 21:45:44 by felayan           #+#    #+#             */
+/*   Updated: 2025/02/18 21:45:45 by felayan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static void	reading(int fd, char **rem, char *buf)
+{
+	char	*tmp;
+	int		bytesread;
+
+	bytesread = 1;
+	while (bytesread > 0)
+	{
+		bytesread = read(fd, buf, BUFFER_SIZE);
+		if (bytesread < 0)
+		{
+			free(*rem);
+			*rem = NULL;
+			return ;
+		}
+		buf[bytesread] = 0;
+		if (bytesread == 0)
+			break ;
+		if (!*rem)
+			*rem = ft_strdup("");
+		tmp = *rem;
+		*rem = ft_strjoin(tmp, buf);
+		free (tmp);
+		if (ft_strchr(buf, '\n'))
+			break ;
+	}
+}
+
+static char	*extract(const char *rem)
+{
+	int		len;
+	int		i;
+	char	*line;
+
+	i = 0;
+	len = lentonew(rem);
+	line = malloc(len + 1);
+	if (!line)
+		return (NULL);
+	while (i < len)
+	{
+		line[i] = rem[i];
+		i++;
+	}
+	line[i] = 0;
+	return (line);
+}
+
+static char	*set_next_line(char *rem)
+{
+	char	*new_pos;
+	char	*tmp;
+
+	new_pos = ft_strchr(rem, '\n');
+	if (new_pos)
+	{
+		tmp = ft_strdup(new_pos + 1);
+		if (!tmp)
+		{
+			free(rem);
+			return (NULL);
+		}
+		free(rem);
+		rem = tmp;
+	}
+	else
+	{
+		free(rem);
+		rem = NULL;
+	}
+	return (rem);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remain;
+	char		*buf;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+	{
+		free(remain);
+		remain = NULL;
+		return (NULL);
+	}
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	reading(fd, &remain, buf);
+	free(buf);
+	buf = NULL;
+	if (!remain || !*remain)
+	{
+		free(remain);
+		remain = NULL;
+		return (NULL);
+	}
+	line = extract(remain);
+	remain = set_next_line(remain);
+	return (line);
+}
---
+++ Core/GNL/get_next_line.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: felayan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 21:46:08 by felayan           #+#    #+#             */
+/*   Updated: 2025/02/18 21:46:09 by felayan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_strdup(const char *s);
+char	*ft_strchr(const char *s, int c);
+int		ft_strlen(const char *s);
+int		lentonew(const char *s);
+
+#endif
